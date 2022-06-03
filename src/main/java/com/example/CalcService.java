f39756920932782f2603fec4da9e61f89fa8c478
kodlan <6258615+kodlan@users.noreply.github.com>
2022-06-03 14:40:26 +0300

    reformatted to google code style

--- src/main/java/com/example/CalcService.java
+++ src/main/java/com/example/CalcService.java
@@ -7,50 +7,51 @@ import java.util.List;
 
 public class CalcService extends CalcServiceGrpc.CalcServiceImplBase {
 
-    @Override
-    public void calc(CalcRequest request, StreamObserver<CalcResponse> responseObserver) {
-        int num1 = request.getNum1();
-        int num2 = request.getNum2();
-        CalcRequest.Operation op = request.getOperation();
+  @Override
+  public void calc(CalcRequest request, StreamObserver<CalcResponse> responseObserver) {
+    int num1 = request.getNum1();
+    int num2 = request.getNum2();
+    CalcRequest.Operation op = request.getOperation();
 
-        int res = switch (op) {
-            case SUM -> num1 + num2;
-            case SUB -> num1 - num2;
-            case MUL -> num1 * num2;
-            case DIV -> num1 / num2;
-            default -> 0;
-        };
+    int res = switch (op) {
+      case SUM -> num1 + num2;
+      case SUB -> num1 - num2;
+      case MUL -> num1 * num2;
+      case DIV -> num1 / num2;
+      default -> 0;
+    };
 
-        CalcResponse response = CalcResponse.newBuilder()
-                .setResult(res)
-                .build();
+    CalcResponse response = CalcResponse.newBuilder()
+        .setResult(res)
+        .build();
 
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void prime(PrimeRequest request, StreamObserver<PrimeResponse> responseObserver) {
+    List<Integer> primes = getPrimes(request.getNumber());
+
+    primes.forEach(
+        prime -> responseObserver.onNext(PrimeResponse.newBuilder().setPrime(prime).build()));
+
+    responseObserver.onCompleted();
+  }
+
+  private List<Integer> getPrimes(int number) {
+    List<Integer> primes = new ArrayList<>();
+
+    int k = 2;
+    while (number > 1) {
+      if (number % k == 0) {
+        primes.add(k);
+        number = number / k;
+      } else {
+        k++;
+      }
     }
 
-    @Override
-    public void prime(PrimeRequest request, StreamObserver<PrimeResponse> responseObserver) {
-        List<Integer> primes = getPrimes(request.getNumber());
-
-        primes.forEach(prime -> responseObserver.onNext(PrimeResponse.newBuilder().setPrime(prime).build()));
-
-        responseObserver.onCompleted();
-    }
-
-    private List<Integer> getPrimes(int number) {
-        List<Integer> primes = new ArrayList<>();
-
-        int k = 2;
-        while (number > 1) {
-            if (number % k == 0) {
-                primes.add(k);
-                number = number / k;
-            } else {
-                k ++;
-            }
-        }
-
-        return primes;
-    }
+    return primes;
+  }
 }
